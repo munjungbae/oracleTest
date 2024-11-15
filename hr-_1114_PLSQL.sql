73929a9a8ed108cb4029a29556fa9f7bdf44b7aa
munjungbae <ster12390@naver.com>
2024-11-15 11:41:02 +0900

    PL/SQL  EMPLOYEES 테이블에서 요구한 부서별 사용자 정보(이름, 월급)를 CURSOR에 저장하고 부서별 요청 시 해당되는 부서정보를 출력 테스트 생성.

--- hr-_1114_PLSQL.sql
+++ hr-_1114_PLSQL.sql
@@ -273,6 +273,51 @@ BEGIN
 ****************************************/
 END;
 /
+--EMPLOYEES 테이블에서 요구한 부서별 사용자 정보(이름, 월급)를 CURSOR에 저장하고 부서별 요청 시 해당되는 부서정보를 출력
+DECLARE
+    VEMPROW_TYPE EMPLOYEES%ROWTYPE;
+    VSALARY VARCHAR(10);
+    VNO NUMBER(4);
+    --부서별로 정보를 저장 할 수 있는 커서를 생성
+    CURSOR C1 (VDEPTNO EMPLOYEES.DEPARTMENT_ID%TYPE)
+    IS SELECT * FROM EMPLOYEES WHERE DEPARTMENT_ID = VDEPTNO;
+BEGIN
+    --부서 별 정보를 생성, 랜덤 값으로.
+    VNO := ROUND(DBMS_RANDOM.VALUE(10,110),-1);
+    IF(VNO = 40) THEN
+        DBMS_OUTPUT.PUT_LINE(VNO || '는 해당되지 않는 번호입니다.');
+        RETURN;
+    END IF;
+    --부서 별 정보를 가져와 월급에 대해 평가하기.
+    FOR VEMPROW_TYPE IN C1(VNO) LOOP
+        IF VEMPROW_TYPE.SALARY BETWEEN 1 AND 1000 THEN
+            VSALARY := '낮음';
+        ELSIF VEMPROW_TYPE.SALARY BETWEEN 1001 AND 2000 THEN
+            VSALARY := '중간';
+        ELSIF VEMPROW_TYPE.SALARY BETWEEN 2001 AND 3000 THEN
+            VSALARY := '높음';
+        ELSE
+            VSALARY := '최고';
+        END IF;
+        DBMS_OUTPUT.PUT_LINE(VNO|| ' / ' || VEMPROW_TYPE.FIRST_NAME || ' / ' || VEMPROW_TYPE.SALARY || ' / ' || VSALARY);
+    END LOOP;
+END;
+/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
