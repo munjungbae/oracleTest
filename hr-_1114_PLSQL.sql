6113101509b6f3eaa12cf7c7d0004aaeccd7090e
munjungbae <ster12390@naver.com>
2024-11-15 10:16:06 +0900

    반복문을 사용하여 오라클내 구구단 작성하기

--- hr-_1114_PLSQL.sql
+++ hr-_1114_PLSQL.sql
@@ -230,7 +230,19 @@ BEGIN
 END;
 /
 
-
+DECLARE
+    I NUMBER(2);
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('구구단');
+    DBMS_OUTPUT.PUT_LINE('------------------');
+    FOR I IN 1..9 LOOP
+        FOR J IN 1..9 LOOP
+            DBMS_OUTPUT.PUT_LINE( I || ' X ' || J || ' = ' || I*J);
+        END LOOP;
+        DBMS_OUTPUT.PUT_LINE('------------------');
+    END LOOP;
+END;
+/
 
 
 
