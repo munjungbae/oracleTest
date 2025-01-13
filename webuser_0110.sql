6cea700be1bd5d89cebaee52794bb227f25bcf6e
munjungbae <ster12390@naver.com>
2025-01-13 16:07:07 +0900

    mybatis 활용 테이블 생성

--- webuser_0110.sql
+++ webuser_0110.sql
@@ -27,3 +27,39 @@ create sequence jpaboard_seq
 start with 1
 increment by 1;
 commit;
+
+
+CREATE TABLE mybatisboard
+(
+    board_no NUMBER,
+    title VARCHAR2(100) NOT NULL,   
+    content VARCHAR2(500) NULL,
+    writer VARCHAR2(50) NOT NULL,
+    reg_date DATE DEFAULT SYSDATE,  
+    PRIMARY KEY (board_no)
+);
+create sequence mybatisboard_seq
+start with 1
+increment by 1;
+
+CREATE TABLE mybatismember(
+    user_no NUMBER,
+    user_id VARCHAR2(50) NOT NULL,
+    user_pw VARCHAR2(50) NOT NULL,
+    user_name VARCHAR2(100) NOT NULL,
+    coin NUMBER(10) DEFAULT 0,
+    reg_date DATE DEFAULT SYSDATE,
+    upd_date DATE DEFAULT SYSDATE,
+    enabled CHAR(1) DEFAULT '1',
+    PRIMARY KEY (user_no)
+);
+CREATE TABLE mybatismember_auth(
+    user_no NUMBER NOT NULL, 
+    auth VARCHAR2(50) NOT NULL
+);
+ALTER TABLE mybatismember_auth ADD CONSTRAINT fk_mybatismember_auth_user_no
+FOREIGN KEY(user_no) REFERENCES mybatismember(user_no);
+
+create sequence mybatismember_seq
+start with 1
+increment by 1;
